b0e097550f04bed06e6d44d0d6b842433aa24f72
P.S.V.R <pmq2001@gmail.com>
2012-04-24 13:18:20 +0800

    init with steak

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thebooks::Application.config.secret_token = '32927bc0b9d15e9c03128d32896dbc54c3a4608c635bd2307a2aad2e9c06e178549b0ca6761e1d189538dfd55934ebc6537f48e5d2a5953a4841e94ef6793bff'
